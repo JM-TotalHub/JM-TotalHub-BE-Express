980fa3f03a0e3d296a5cbea58b7bac5ae588b54c
jundev95 <junmop950dev@gmail.com>
2024-08-11 18:53:21 +0900

    cors 수정

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://15.164.36.109',
+    origin: 'http://10.1.10.191',
+    // origin: 'http://15.164.36.109',
     // origin: `http://${ENV.REACT_LOCAL_HOST}:${ENV.REACT_LOCAL_PORT}`,
     credentials: true,
   })
