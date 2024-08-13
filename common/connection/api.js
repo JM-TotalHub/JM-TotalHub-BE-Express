f1f9908ae87803fe7e216ec9a9ae1e5a950f71d4
jundev95 <junmop950dev@gmail.com>
2024-08-13 17:00:11 +0900

    환경변수 수정

--- common/connection/api.js
+++ common/connection/api.js
@@ -7,7 +7,10 @@ const api = (() => {
   // const NGINX_SERVER_EC2_HOST = ENV.NGINX_SERVER_EC2_HOST;
 
   const signalApi = axios.create({
-    baseURL: `${ENV.NGINX_SERVER_EC2_HOST}/signal`,
+    baseURL:
+      ENV.isProd === 'pord'
+        ? `${ENV.NGINX_SERVER_EC2_HOST}/signal`
+        : `${ENV.NGINX_SERVER_EC2_HOST}`,
   });
 
   const setupInterceptors = () => {
